2b573d9135d3754637a6ed0055b2b23003b72442
jeje <jeje@localhost.localdomain>
2020-12-20 20:53:42 +0100

    ajout de la clef pour l'api

--- js/weather.js
+++ js/weather.js
@@ -4,14 +4,10 @@ const descElement = document.querySelector('.temperature-description p');
 
 // App data
 const weather = {};
-weather.temperature = {
-    unit: 'celsius',
-};
 
 // Change to 'F' for Fahrenheit
 var tempUnit = 'C';
 
-const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
 const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
 
@@ -19,17 +15,17 @@ const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
 setPosition();
 
 function setPosition(position) {
-    // Here you can change your position
-    // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+
+    //https://www.latlong.net/
+    let latitude = 46.233124;
+    let longitude = 7.360626;
 
     getWeather(latitude, longitude);
 }
 
 // Get the Weather data
 function getWeather(latitude, longitude) {
-    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
 
     console.log(api);
 
@@ -39,8 +35,8 @@ function getWeather(latitude, longitude) {
             return data;
         })
         .then(function (data) {
-            let celsius = Math.floor(data.main.temp - KELVIN);
-            weather.temperature.value = (tempUnit == 'C') ? celsius : (celsius * 9/5) + 32;
+
+            weather.temperature.value = data.main.temp;
             weather.description = data.weather[0].description;
             weather.iconId = data.weather[0].icon;
         })
